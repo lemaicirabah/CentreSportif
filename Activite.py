fc95916f0da4ad02a4b1bf2d3bd6bfe5ad106447
lemaicirabah <lemaicirabah@gmail.com>
2024-03-28 12:36:09 -0400

    Update files

--- Activite.py
+++ Activite.py
@@ -1,21 +1,13 @@
+from database import execute_query
+
 class Activite:
-    def __init__(self, nom, description, horaires, capacite_max):
-        self.nom = nom
-        self.description = description
-        self.horaires = horaires
-        self.capacite_max = capacite_max
-        self.inscrits = []
+    @staticmethod
+    def get_activities():
+            query = "SELECT activity_id, name FROM activities"
+            activities = execute_query(query).fetchall()
+            return activities
 
-    def ajouter_inscription(self, client):
-        if len(self.inscrits) < self.capacite_max:
-            self.inscrits.append(client)
-            return True
-        else:
-            return False
-
-    def supprimer_inscription(self, client):
-        if client in self.inscrits:
-            self.inscrits.remove(client)
-            return True
-        else:
-            return False
+    @staticmethod
+    def insert_activities(activities_to_insert):
+            query = "INSERT INTO activities (name, description, min_participants, max_participants) VALUES (?, ?, ?, ?)"
+            execute_query(query, activities_to_insert)
--- Client.py
+++ Client.py
@@ -1,34 +1,17 @@
+from database import execute_query
+
 class Client:
-    def __init__(self, matricule, nom, prenom, adresse, email, mot_de_passe):
-        self.matricule = matricule
-        self.nom = nom
-        self.prenom = prenom
-        self.adresse = adresse
-        self.email = email
-        self.mot_de_passe = mot_de_passe
-    def inscrire_activite(self, activite):
-        # Méthode pour inscrire le client à une activité
-        pass
+    def __init__(self, user_id=None):
+        self.user_id = user_id
 
-    def annuler_inscription(self, activite):
-        # Méthode pour annuler l'inscription du client à une activité
-        pass
+    @staticmethod
+    def verify_user(username, password):
+        query = "SELECT user_id FROM users WHERE username=? AND password=?"
+        user = execute_query(query, (username, password)).fetchone()
+        return user[0] if user else None
 
-    def consulter_facture(self):
-        # Méthode pour consulter la facture personnelle du client
-        pass
-
-    def consulter_horaire(self):
-        # Méthode pour consulter l'horaire personnel des activités du client
-        pass
-
-    def modifier_profil(self, nouveau_nom, nouvelle_adresse, nouvel_email, nouveau_mot_de_passe):
-        # Méthode pour modifier le profil du client
-        self.nom = nouveau_nom
-        self.adresse = nouvelle_adresse
-        self.email = nouvel_email
-        self.mot_de_passe = nouveau_mot_de_passe
-
-    def consulter_infos_activites(self):
-        # Méthode pour consulter les informations relatives aux activités du centre
-        pass
+    def register_activity(self, activity_id):
+        if not self.user_id:
+            raise Exception("Client not logged in.")
+        query = "INSERT INTO registrations (user_id, activity_id, status) VALUES (?, ?, 'registered')"
+        execute_query(query, (self.user_id, activity_id))
