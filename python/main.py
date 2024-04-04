62ecd0cad7ea96b1219a8a162116b320dced7eb7
Mbouchelkia <med.bouchelkia@gmail.com>
2024-04-04 02:12:12 -0400

    Changement du design de main_frame

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
+from tkinter import *
 from client import Client
 from accueil import Accueil
 import database
@@ -8,16 +9,21 @@ class SportCenterApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Sport Center")
-        self.configure(bg='#232F34')
+        self.iconbitmap('icon.ico')
         self.geometry('400x500')
-       # self.resizable(width=False, height=False)
+        self.resizable(width=False, height=False)
 
         self.main_frame = ttk.Frame(self, style='My.TFrame')
         self.main_frame.pack(fill=tk.BOTH, expand=True)
 
+        original_image = tk.PhotoImage(file='gradient.png')
+        resized_image = original_image.subsample(4)
+        self.bg_label = tk.Label(self.main_frame, image=resized_image)
+        self.bg_label.image = resized_image
+        self.bg_label.place(relx=0, rely=0, relwidth=1, relheight=1)
+
         style = ttk.Style(self)
-        style.configure('My.TFrame', background='#232F34')
-        style.configure('My.TLabel', background='#232F34', foreground='#FFFFFF')
+        style.configure('My.TLabel', background='#30c7cb', foreground='#000000')
         style.configure('My.TButton', font=('Arial', 12, 'bold'), padding=10, foreground='#000000',
                         background='black', width='8')
 
@@ -30,7 +36,6 @@ class SportCenterApp(tk.Tk):
         welcome_label.grid(row=0, column=0, pady=0, padx=10, sticky="EW")
         welcome_label.place(relx=0.5, rely=0.05, anchor=tk.CENTER, width=400)
 
-
         login_button = ttk.Button(self.main_frame, text="Login", command=self.show_login_window, style="My.TButton")
         login_button.grid(row=1, column=0, padx=10, pady=10,  sticky="EW")
         login_button.place(relx=0.5, rely=0.3, anchor=tk.CENTER, width=200)
@@ -42,24 +47,31 @@ class SportCenterApp(tk.Tk):
 
     def show_login_window(self):
         login_win = tk.Toplevel(self)
-        frame = ttk.Frame(login_win)
         login_win.title("Login")
-        ttk.Label(frame, text="Login", font=("Arial", 20)).grid(row=0, column=1, padx=10, pady=10)
-        login_win.geometry("600x600")
+        login_win.geometry("400x500")
+        login_win.resizable(width=False, height=False)
+        login_win.iconbitmap('icon.ico')
 
-        ttk.Label(frame, text="Username:", font=("Arial", 16)).grid(row=1, column=0, padx=10, pady=10)
-        username_entry = ttk.Entry(frame, font=("Arial", 16))
-        username_entry.grid(row=1, column=1, padx=10, pady=10)
+        original_image = tk.PhotoImage(file='gradient.png')
+        resized_image = original_image.subsample(4)
 
-        ttk.Label(frame, text="Password:", font=("Arial", 16)).grid(row=2, column=0, padx=10, pady=10)
-        password_entry = ttk.Entry(frame, show="*", font=("Arial", 16))
-        password_entry.grid(row=2, column=1, padx=10, pady=10)
+        bg_label = tk.Label(login_win, image=resized_image)
+        bg_label.image = resized_image
+        bg_label.place(relx=0, rely=0, relwidth=1, relheight=1)
 
-        ttk.Button(frame, text="Login", command=lambda: self.verify_login(
-            username_entry.get(), password_entry.get(), login_win, self), style="My.TButton").grid(row=3, column=1,
-                                                                                                   columnspan=2,
-                                                                                                   pady=10)
-        frame.pack()
+        title = ttk.Label(bg_label, style="My.TLabel", text="Enter your username\nand password to log in", font=("Arial", 16))
+        title.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+        ttk.Label(bg_label, text="Username:", font=("Arial", 14)).place(relx=0.15, rely=0.3, anchor=tk.CENTER)
+
+        username_entry = ttk.Entry(bg_label, font=("Arial", 14))
+        username_entry.place(relx=0.55, rely=0.3, anchor=tk.CENTER)
+
+        ttk.Label(bg_label, text="Password:", font=("Arial", 14)).place(relx=0.15, rely=0.4, anchor=tk.CENTER)
+        password_entry = ttk.Entry(bg_label, show="*", font=("Arial", 14))
+        password_entry.place(relx=0.55, rely=0.4, anchor=tk.CENTER)
+        ttk.Button(bg_label, text="Login", command=lambda: self.verify_login(
+            username_entry.get(), password_entry.get(), login_win, self), style="My.TButton").place(relx=0.5, rely=0.5,
+                                                                                                    anchor=tk.CENTER)
 
     def verify_login(self, username, password, window, window1):
         user_id = Client.verify_user(username, password)
